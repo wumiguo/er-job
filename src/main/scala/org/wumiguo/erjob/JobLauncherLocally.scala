9167403ae20972586e1efb3567376b82c5a3995d
levinliu <levinmhliu@gmail.com>
2020-09-02 08:01:34 +0800

    add job launcher

--- src/main/scala/org/wumiguo/erjob/JobLauncherLocally.scala
+++ src/main/scala/org/wumiguo/erjob/JobLauncherLocally.scala
@@ -15,7 +15,7 @@ import org.wumiguo.ser.methods.util.CommandLineUtil
  *         Created on 2020/7/15
  *         (Change file header on Settings -> Editor -> File and Code Templates)
  */
-object JobLauncher extends SparkEnvSetup {
+object JobLauncherLocally extends SparkEnvSetup {
 
   def main(args: Array[String]): Unit = {
     log.info("start to run er job")
@@ -26,7 +26,11 @@ object JobLauncher extends SparkEnvSetup {
     val output = erJobConf.getOutput
     val sourcePairs = erJobConf.getSourcesPairs
     var sourceCounter = 0
-    val spark = createSparkSession(getClass.getName)
+    val outputDir = new File("/tmp/data-er")
+    if (!outputDir.exists()) {
+      outputDir.mkdirs()
+    }
+    val spark = createLocalSparkSession(getClass.getName, outputDir = output.path)
     var statPathArr = Array[String]()
     val flowConfPath = CommandLineUtil.getParameter(args, "flowConfPath", "src/main/resources/flows-configuration.yml")
     val flowsConf = FlowsConfigurationLoader.load(flowConfPath)
