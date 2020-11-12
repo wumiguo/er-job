64af3c2cd8ea0fa65d96506a384f8b865d855160
levinliu <levinmhliu@gmail.com>
2020-11-12 16:12:21 +0800

    add sample configuration on er

---
+++ src/main/scala/org/wumiguo/erjob/JobLauncherNoIdLocally.scala
@@ -0,0 +1,34 @@
+package org.wumiguo.erjob
+
+import java.io.File
+
+import org.wumiguo.erjob.io.{ApplicationConfigurationLoader, ERJobConfigurationLoader}
+import org.wumiguo.ser.common.{SparkAppConfigurationSupport, SparkEnvSetup}
+import org.wumiguo.ser.methods.util.CommandLineUtil
+
+
+/**
+ * @author levinliu
+ *         Created on 2020/7/15
+ *         (Change file header on Settings -> Editor -> File and Code Templates)
+ */
+object JobLauncherNoIdLocally extends SparkEnvSetup {
+
+  def main(args: Array[String]): Unit = {
+    log.info("start to run er job locally")
+    val outputDir = new File("/tmp/data-er")
+    if (!outputDir.exists()) {
+      outputDir.mkdirs()
+    }
+    val appConfPath = CommandLineUtil.getParameter(args, "appConfPath", "src/main/resources/application-local.yml")
+    val sparkConf = ApplicationConfigurationLoader.loadSparkConf(appConfPath)
+    //val jobConfPath = CommandLineUtil.getParameter(args, "jobConfPath", "src/main/resources/er-job-configuration-one-source-pair-noid.yml")
+    val jobConfPath = CommandLineUtil.getParameter(args, "jobConfPath", "src/main/resources/er-job-configuration-noid.yml")
+    val erJobConf = ERJobConfigurationLoader.load(jobConfPath)
+    val output = erJobConf.getOutput
+    val spark = createSparkSession(getClass.getName, sparkConf)
+    val mergeArgs = args ++ SparkAppConfigurationSupport.sparkConf2Args(sparkConf)
+    log.info("args=" + mergeArgs.toList)
+    BaseJobLauncher.main(mergeArgs)
+  }
+}
